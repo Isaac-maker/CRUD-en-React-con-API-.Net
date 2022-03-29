7005a73260b1357579270071022a88aa26e3db70
Isaac-maker <65924751+Isaac-maker@users.noreply.github.com>
2022-03-29 00:17:15 -0600

    endopoints

--- src/App.js
+++ src/App.js
@@ -1,13 +1,81 @@
-import React from "react";
+import React,{useState} from "react";
+import contants from "./utilities/contants"
 
 export default function App() {
+  
+  const [posts,setPosts]=useState([]);
+
+  function getPosts(){
+    const url= contants.API_GET_ALL_POSTS;
+
+    fetch(url,{
+      method: 'GET'
+    }).then(response => response.json())
+    .then(postsFromserver =>{
+      setPosts(postsFromserver);
+    }).catch((error)=>{
+      console.log(error);
+      alert(error);
+    });
+  }
+
+  
   return (
     <div className="container">
       <div className="row min-vh-100">
-        <div className="col d-flex flex-colum justify-content-center align-items-center">
-          <h1>Entonces</h1>
+        <div className="col d-flex flex-column justify-content-center align-items-center">
+          <div>
+          <h1>Crud</h1>
+          <div className="mt-5">
+            <button onClick={getPosts} className="btn btn-primary btn-lg w-100">get</button>
+            <button onClick={()=>{}} className="btn btn-success btn-lg w-100 mt-4">Crear nuevo post</button>
+          </div>
+          </div>
+          
+          {posts.length > 0 && renderPostTble()}
         </div>
       </div>
     </div>
   );
+
+  function renderPostTble() {
+    return (
+      <div className="table-responsive mt-5">
+        <table className="table table-bordered border-success">
+          <thead>
+            <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Titulo</th>
+              <th scope="col">Contenido</th>
+              <th scope="col">Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            {posts.map((posts)=>(
+            <tr key={posts.id}>
+              <th scope="row">{posts.id}</th>
+              <th>{posts.title}</th>
+              <th>{posts.content}</th>
+              <td>
+                <button className="btn btn-warning btn-lg mx-3 my-3">
+                  Actualizar
+                </button>
+                <button className="btn btn-danger btn-lg mx-3 my-3">
+                  Eliminar
+                </button>
+              </td>
+            </tr> ))}
+          
+          </tbody>
+        </table>
+        <button onClick={()=>setPosts([])} className="btn btn-success btn-lg w-100  ">Posts React</button>
+        
+      </div>
+   
+    );
+    
+  }
+
+  
 }
---
+++ src/utilities/contants.js
@@ -0,0 +1,31 @@
+const API_DEVELOPMENT='https://localhost:7167';
+const API_PRODUCTION='https://appname.arurewebsites.net';
+
+const endopoints ={
+
+    get_all_post: 'get-all-posts',
+    get_post_id: 'get-all-posts-by-id',
+    create_post:'create-post',
+    update_post:'update-post',
+    delete_post:'delete-post'
+};
+
+const development={
+    API_GET_ALL_POSTS: `${API_DEVELOPMENT}/${endopoints.get_all_post}`,
+    API_GET_ID_POSTS:`${API_DEVELOPMENT}/${endopoints.get_post_id}`,
+    API_CREATE_POSTS:`${API_DEVELOPMENT}/${endopoints.create_post}`,
+    API_UPDATE_POSTS:`${API_DEVELOPMENT}/${endopoints.update_post}`,
+    API_DELETE_POSTS:`${API_DEVELOPMENT}/${endopoints.delete_post}`,
+};
+
+const production={
+    API_GET_ALL_POSTS:`${API_PRODUCTION}/${endopoints.get_all_post}`,
+    API_GET_ID_POSTS:`${API_PRODUCTION}/${endopoints.get_post_id}`,
+    API_CREATE_POSTS:`${API_PRODUCTION}/${endopoints.create_post}`,
+    API_UPDATE_POSTS:`${API_PRODUCTION}/${endopoints.update_post}`,
+    API_DELETE_POSTS:`${API_PRODUCTION}/${endopoints.delete_post}`,
+};
+
+const constants = process.env.NODE_ENV === 'development' ? development : production;
+
+export default constants;
